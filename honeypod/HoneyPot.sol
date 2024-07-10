44f030097bb34f9c0437e03c77556e63d09d1e0e
ikuta1011 <ikuta1011@gmail.com>
2024-07-10 15:15:18 +0900

    Update HoneyPot.sol

--- honeypod/HoneyPot.sol
+++ honeypod/HoneyPot.sol
@@ -1,18 +1,1 @@
-contract HoneyPot {
-    mapping (address => uint) public balances;
-    function HoneyPot() payable {
-        put();
-    }
-    function put() payable {
-        balances[msg.sender] = msg.value;
-    }
-    function get() {
-        if (!msg.sender.call.value(balances[msg.sender])()) {
-            throw;
-        }
-        balances[msg.sender] = 0;
-    }
-    function() {
-        throw;
-    }
-}
+
